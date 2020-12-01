782dd795d6f47d123d036e717e38a65815cb66f5
gulistanay <53774762+gulistanay@users.noreply.github.com>
2020-12-01 13:20:00 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Soru_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonKoordinat = new System.Windows.Forms.Button();
+            this.buttonHesapla = new System.Windows.Forms.Button();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.listBoxDegerler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonKoordinat
+            // 
+            this.buttonKoordinat.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonKoordinat.Location = new System.Drawing.Point(124, 243);
+            this.buttonKoordinat.Name = "buttonKoordinat";
+            this.buttonKoordinat.Size = new System.Drawing.Size(152, 59);
+            this.buttonKoordinat.TabIndex = 4;
+            this.buttonKoordinat.Text = "KOORDİNAT GİR";
+            this.buttonKoordinat.UseVisualStyleBackColor = true;
+            this.buttonKoordinat.Click += new System.EventHandler(this.buttonKoordinat_Click);
+            // 
+            // buttonHesapla
+            // 
+            this.buttonHesapla.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonHesapla.Location = new System.Drawing.Point(140, 333);
+            this.buttonHesapla.Name = "buttonHesapla";
+            this.buttonHesapla.Size = new System.Drawing.Size(118, 34);
+            this.buttonHesapla.TabIndex = 5;
+            this.buttonHesapla.Text = "HESAPLA";
+            this.buttonHesapla.UseVisualStyleBackColor = true;
+            this.buttonHesapla.Click += new System.EventHandler(this.buttonHesapla_Click);
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxX.Location = new System.Drawing.Point(207, 83);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(125, 28);
+            this.textBoxX.TabIndex = 0;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxY.Location = new System.Drawing.Point(207, 170);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(125, 28);
+            this.textBoxY.TabIndex = 1;
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelX.Location = new System.Drawing.Point(45, 83);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(121, 22);
+            this.labelX.TabIndex = 2;
+            this.labelX.Text = "X Koordinatı:";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelY.Location = new System.Drawing.Point(46, 173);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(120, 22);
+            this.labelY.TabIndex = 3;
+            this.labelY.Text = "Y Koordinatı:";
+            // 
+            // listBoxDegerler
+            // 
+            this.listBoxDegerler.FormattingEnabled = true;
+            this.listBoxDegerler.ItemHeight = 16;
+            this.listBoxDegerler.Location = new System.Drawing.Point(388, 114);
+            this.listBoxDegerler.Name = "listBoxDegerler";
+            this.listBoxDegerler.Size = new System.Drawing.Size(120, 84);
+            this.listBoxDegerler.TabIndex = 6;
+            this.listBoxDegerler.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 453);
+            this.Controls.Add(this.listBoxDegerler);
+            this.Controls.Add(this.buttonHesapla);
+            this.Controls.Add(this.buttonKoordinat);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonKoordinat;
+        private System.Windows.Forms.Button buttonHesapla;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.ListBox listBoxDegerler;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soru_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        int bolge1 = 0, bolge2 = 0, bolge3 = 0, bolge4 = 0;
+
+        private void buttonKoordinat_Click(object sender, EventArgs e)
+        {
+            if (textBoxX.Text == "" || textBoxY.Text == "")
+            {
+                MessageBox.Show("Lütfen koordinat girin.");
+            }
+            else
+            {
+                int x = Convert.ToInt32(textBoxX.Text);
+                int y = Convert.ToInt32(textBoxY.Text);
+
+                if (x > 0 && y > 0)     
+                    bolge1++;
+                else if (x < 0 && y > 0)
+                    bolge2++;
+                else if (x < 0 && y < 0)
+                    bolge3++;
+                else if( x > 0 && y < 0)
+                    bolge4++;
+                else
+                    MessageBox.Show("0 noktası hiçbir bölgeye ait değildir.");  //0 olma durumunda hiçbir bölgeye ait olunmayacağı için 0 değerleri dahil edilmedi.
+                textBoxX.Clear();       //Temizleme işlemi
+                textBoxY.Clear();
+                textBoxX.Focus();       //İmleç yeniden konumlandırıldı
+            }
+        }
+
+        private void buttonHesapla_Click(object sender, EventArgs e)
+        {
+            //listBoxDegerler.Items.Add("1.bölge= " + bolge1);        //Listboxda düzenli görünüm ile kontrol sağlandı
+            //listBoxDegerler.Items.Add("2.bölge= " + bolge2);
+            //listBoxDegerler.Items.Add("3.bölge= " + bolge3);
+            //listBoxDegerler.Items.Add("4.bölge= " + bolge4);
+
+            MessageBox.Show("1.Bölge= " + bolge1.ToString());
+            MessageBox.Show("2.Bölge= " + bolge2.ToString());
+            MessageBox.Show("3.Bölge= " + bolge3.ToString());
+            MessageBox.Show("4.Bölge= " + bolge4.ToString());
+        }
+    }
+}
